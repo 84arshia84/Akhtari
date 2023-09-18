bb0fc6677b5384e4ecb4a3b4282ffe44d10660fc
84arshia84 <arshiia.a84@gmail.com>
2023-09-18 10:16:04 +0330

    add routes/web.php to prajeckt

--- routes/web.php
+++ routes/web.php
@@ -1,5 +1,6 @@
 <?php
 
+use App\Http\Controllers\LoginController;
 use App\Http\Controllers\OpportunityController;
 use App\Http\Controllers\OrderController;
 use Illuminate\Support\Facades\Route;
@@ -22,7 +23,11 @@ Route::get('/', function () {
     return redirect()->route('login');
 });
 
-Route::view('/login', 'authorize.login')->name('login');
+Route::get('/login', function () {
+    return view('authorize.login');
+})->name('login');
+Route::post('/login', [LoginController::class, 'login'])->name('userLogin');
+
 
 Route::view('/register', 'authorize.register')->name('register');
 
@@ -34,7 +39,7 @@ Route::get('/workplace', function () {
 });
 
 Route::prefix('Users')->group(function () {
-    Route::get('/all_data/filtered',[UserController::class,'filterUsers'])->name('filterUsers');
+    Route::get('/all_data/filtered', [UserController::class, 'filterUsers'])->name('filterUsers');
     Route::view('/add_User', 'users.addUser')->name('addUser');
     Route::post('/store_user', [UserController::class, 'store'])->name('store_user');
     Route::get('/all_data', [UserController::class, 'get_all_users'])->name('Users_data');
@@ -55,26 +60,24 @@ Route::prefix('Product')->group(function () {
         ->name('store_edit_product');
 });
 
-Route::prefix('Opportunity')->group(function ()
-{
-    Route::view('/add_Opportunity','opportunitys.addOpportunity')->name('addOpportunity');
-    Route::post('/store_opportunities',[OpportunityController::class,'store'])->name('store_opportunities');
-    Route::GET('/all_opportunities' , [OpportunityController::class,'get_all_opportunities'])->name('opportunities_data');
-    Route::GET('/edit_opportunities{id}', [OpportunityController::class,'edit_opportunities'])->name('edit_opportunities');
-    Route::post('/store_edit_opportunities{id}', [OpportunityController::class,'store_edit_opportunities'])->name('store_edit_opportunities');
-    Route::post('/delete_opportunities/{id}', [OpportunityController::class,'delete_opportunities'])->name('opportunities_delete');
-    Route::view('/Opportunitys','opportunitys.opportunitysData')->name('Opportunitys_data');
+Route::prefix('Opportunity')->group(function () {
+    Route::view('/add_Opportunity', 'opportunitys.addOpportunity')->name('addOpportunity');
+    Route::post('/store_opportunities', [OpportunityController::class, 'store'])->name('store_opportunities');
+    Route::GET('/all_opportunities', [OpportunityController::class, 'get_all_opportunities'])->name('opportunities_data');
+    Route::GET('/edit_opportunities{id}', [OpportunityController::class, 'edit_opportunities'])->name('edit_opportunities');
+    Route::post('/store_edit_opportunities{id}', [OpportunityController::class, 'store_edit_opportunities'])->name('store_edit_opportunities');
+    Route::post('/delete_opportunities/{id}', [OpportunityController::class, 'delete_opportunities'])->name('opportunities_delete');
+    Route::view('/Opportunitys', 'opportunitys.opportunitysData')->name('Opportunitys_data');
 
 });
 
-Route::prefix('Check')->group(function ()
-{
-    Route::get('/add_Check',[CheckController::class,'addCheck'])->name('addCheck');
+Route::prefix('Check')->group(function () {
+    Route::get('/add_Check', [CheckController::class, 'addCheck'])->name('addCheck');
     Route::post('/submit_Check', [CheckController::class, 'create'])->name('submitCheck');
-    Route::get('/Checks',[CheckController::class,'get_all_checks'])->name('Checks_data');
-    Route::post('/delete_check/{id}',[CheckController::class,'delete_check'])->name('delete_check');
-    Route::post('/edit_check/{id}',[CheckController::class,'store_edited_check'])->name('store_edited_check');
-    Route::get('/edit_check/{id}',[CheckController::class,'editCheck'])->name('edit_check');
+    Route::get('/Checks', [CheckController::class, 'get_all_checks'])->name('Checks_data');
+    Route::post('/delete_check/{id}', [CheckController::class, 'delete_check'])->name('delete_check');
+    Route::post('/edit_check/{id}', [CheckController::class, 'store_edited_check'])->name('store_edited_check');
+    Route::get('/edit_check/{id}', [CheckController::class, 'editCheck'])->name('edit_check');
 
 });
 
@@ -82,13 +85,11 @@ Route::prefix('Order')->group(function () {
     Route::get('/Orders', [OrderController::class, 'getAllOrders'])->name('Orders_data');
     Route::post('/delete_order/{id}', [OrderController::class, 'deleteOrder'])->name('delete_order');
     Route::post('/store_Order', [OrderController::class, 'addOrder'])->name('store_order');
-    Route::get('/add_Order', [OrderController::class,'addOrderPage'])->name('addOrder');
+    Route::get('/add_Order', [OrderController::class, 'addOrderPage'])->name('addOrder');
     Route::get('edite order/{id}', [OrderController::class, 'editOrder'])->name('edite_user');
     Route::post('/storing_editedOrder/{id}', [OrderController::class, 'saveEditedOrder'])
         ->name('save_edited_order');
 });
-
-# GET route for Data options views
 
 
 #profile edit routs
