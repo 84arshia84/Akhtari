49587a9a5c38c28f5a3df795f3428afcb74e83c0
84arshia84 <arshiia.a84@gmail.com>
2023-09-28 15:23:30 +0330

    app/Http/Controllers/OrderController.php

--- app/Http/Controllers/OrderController.php
+++ app/Http/Controllers/OrderController.php
@@ -2,9 +2,35 @@
 
 namespace App\Http\Controllers;
 
+use App\Models\Order;
 use Illuminate\Http\Request;
 
 class OrderController extends Controller
 {
+    public function add_order(Request $request)
+    {
+        $order = Order::create($request->all());
+        return response()->json([
+            'add_order' => $order
+        ]);
+    }
 
+    public function all_order()
+    {
+        $order=Order::all();
+        return response()->json([
+            $order]);
+    }
+    public function delete_order($id)
+    {
+        Order::destroy($id);
+        return response()->noContent();
+    }
+    public function find_order(Request $request)
+    {
+        $order=Order::find($request->id);
+        return response()->json([
+           'find_order'=>$order
+        ]);
+    }
 }
