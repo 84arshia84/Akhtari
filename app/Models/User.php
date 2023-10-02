a977a08eb94ef4bc68fb08231ae97972a2d2d593
84arshia84 <arshiia.a84@gmail.com>
2023-10-02 11:45:17 +0330

    app/Models/User.php

--- app/Models/User.php
+++ app/Models/User.php
@@ -4,13 +4,15 @@ namespace App\Models;
 
 // use Illuminate\Contracts\Auth\MustVerifyEmail;
 use Illuminate\Database\Eloquent\Factories\HasFactory;
+use Illuminate\Database\Eloquent\Relations\HasMany;
 use Illuminate\Foundation\Auth\User as Authenticatable;
 use Illuminate\Notifications\Notifiable;
 use Laravel\Sanctum\HasApiTokens;
+use Spatie\Permission\Traits\HasRoles;
 
 class User extends Authenticatable
 {
-    use HasApiTokens, HasFactory, Notifiable;
+    use HasApiTokens, HasFactory, Notifiable,HasRoles;
 
     /**
      * The attributes that are mass assignable.
@@ -19,6 +21,8 @@ class User extends Authenticatable
      */
         protected $fillable = [
         'name',
+        'family',
+        'phone_number',
         'email',
         'password',
     ];
@@ -42,4 +46,10 @@ class User extends Authenticatable
         'email_verified_at' => 'datetime',
         'password' => 'hashed',
     ];
+
+    public function orders():HasMany
+    {
+        return$this->hasMany(Order::class);
+    }
+
 }
