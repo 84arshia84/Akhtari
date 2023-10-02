e1d3552642bfbe1ecec65869c29b1df3fdf70206
84arshia84 <arshiia.a84@gmail.com>
2023-10-02 11:43:40 +0330

    app/Http/Controllers/RoleController.php

--- app/Http/Controllers/RoleController.php
+++ app/Http/Controllers/RoleController.php
@@ -3,6 +3,7 @@
 namespace App\Http\Controllers;
 
 use Illuminate\Http\Request;
+use Spatie\Permission\Models\Permission;
 use Spatie\Permission\Models\Role;
 
 class RoleController extends Controller
@@ -10,7 +11,31 @@ class RoleController extends Controller
     public function roles()
     {
         $roles=Role::get();
-
+        return view('role.list',compact('roles'));
+    }
+    public function getCreateRole()
+    {
+        return view('role.create');
+    }
+    public function postCreateRole(Request $request)
+    {
+        Role::create(['name'=>$request->name]);
+        return redirect()->back()->with(['message'=>'Role Created']);
+    }
+    public function getCreatePermission()
+    {
+        return view('permission.create');
+    }
+    public function postCreatePermission(Request$request)
+    {
+        Permission::create(['name'=>$request->name]);
+        return redirect()->back()->with(['message'=>'Permission Create']);
+    }
+    public function Permission()
+    {
+        $permission=Permission::get();
+        return view('permission.list',compact('permission'));
 
     }
+
 }
