53f08fdeed2d20bb833339be1546c05035afcd86
84arshia84 <arshiia.a84@gmail.com>
2023-10-02 11:44:50 +0330

    app/Models/Product.php

--- app/Models/Product.php
+++ app/Models/Product.php
@@ -4,6 +4,7 @@ namespace App\Models;
 
 use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Database\Eloquent\Model;
+use Illuminate\Database\Eloquent\Relations\BelongsTo;
 
 class Product extends Model
 {
@@ -11,12 +12,12 @@ class Product extends Model
 
     protected $fillable = [
         'name',
-        'family',
-        'middle_name',
-        'email',
-        'phone_number',
-        'national_id',
-        'password'
+        'price',
+        'number',
     ];
 
+    public function order():BelongsTo
+    {
+     return$this->belongsTo(Order::class);
+    }
 }
