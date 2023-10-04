fc101dfd0e70d24c1dcc02bc7017ef94994916b3
84arshia84 <arshiia.a84@gmail.com>
2023-10-04 21:03:23 +0330

    resources/views/permission/create.blade.php

--- resources/views/permission/create.blade.php
+++ resources/views/permission/create.blade.php
@@ -1,1 +1,15 @@
-<?php
+{{--@extends('layout.app')--}}
+{{--@section('content')--}}
+{{--    <div class="m-5">--}}
+{{--        <h5>create permission </h5>--}}
+{{--        <form action="{{route('permissions.post.create')}}" method="post">--}}
+{{--            @csrf--}}
+{{--            <div class="form-grope">--}}
+{{--                <label for="">Name </label>--}}
+{{--                <input type="text" name="name" value="{{old('name')}}">--}}
+{{--            </div>--}}
+{{--            <button type="submit" class="btn btn-success">Create</button>--}}
+{{--        </form>--}}
+
+{{--    </div>--}}
+{{--@endsection--}}
