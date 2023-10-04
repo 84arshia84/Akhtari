142444ae223b3b9b243ab07c086d460203da9342
84arshia84 <arshiia.a84@gmail.com>
2023-10-04 21:05:57 +0330

    resources/views/role/list.blade.php

--- resources/views/role/list.blade.php
+++ resources/views/role/list.blade.php
@@ -1,26 +1,26 @@
-@extends('layout.app')
+{{--@extends('layout.app')--}}
 
-@section('content')
-    <div class="m-5">
-        <h5>create <role></role></h5>
-        <table class="table table-responsive">
-            <thead>
-            <tr>
-                <th>name</th>
-            </tr>
-            </thead>
-            <tbody>
-            @foreach($roles as $item)
-                <tr>
-                    <td>
-                        {{$item->name}}
-                    </td>
-                </tr>
-            @endforeach
+{{--@section('content')--}}
+{{--    <div class="m-5">--}}
+{{--        <h5>create <role></role></h5>--}}
+{{--        <table class="table table-responsive">--}}
+{{--            <thead>--}}
+{{--            <tr>--}}
+{{--                <th>name</th>--}}
+{{--            </tr>--}}
+{{--            </thead>--}}
+{{--            <tbody>--}}
+{{--            @foreach($roles as $item)--}}
+{{--                <tr>--}}
+{{--                    <td>--}}
+{{--                        {{$item->name}}--}}
+{{--                    </td>--}}
+{{--                </tr>--}}
+{{--            @endforeach--}}
 
-            </tbody>
-        </table>
+{{--            </tbody>--}}
+{{--        </table>--}}
 
 
-    </div>
-@endsection
+{{--    </div>--}}
+{{--@endsection--}}
