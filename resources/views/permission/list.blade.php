ed3be878e39a8e3796bfe29861f9764482a6e785
84arshia84 <arshiia.a84@gmail.com>
2023-10-04 21:05:02 +0330

    resources/views/permission/list.blade.php

--- resources/views/permission/list.blade.php
+++ resources/views/permission/list.blade.php
@@ -1,1 +1,26 @@
-<?php
+{{--@extends('layout.app')--}}
+
+{{--@section('content')--}}
+{{--    <div class="m-5">--}}
+{{--        <h5>permission list </h5>--}}
+{{--        <table class="table table-responsive">--}}
+{{--            <thead>--}}
+{{--            <tr>--}}
+{{--                <th>name</th>--}}
+{{--            </tr>--}}
+{{--            </thead>--}}
+{{--            <tbody>--}}
+{{--            @foreach($permission as $item)--}}
+{{--                <tr>--}}
+{{--                    <td>--}}
+{{--                        {{$item->name}}--}}
+{{--                    </td>--}}
+{{--                </tr>--}}
+{{--            @endforeach--}}
+
+{{--            </tbody>--}}
+{{--        </table>--}}
+
+
+{{--    </div>--}}
+{{--@endsection--}}
