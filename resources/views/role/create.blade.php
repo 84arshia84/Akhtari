546170c40b9c068bc06bfae4ed49f4a4ae8fb4f3
84arshia84 <arshiia.a84@gmail.com>
2023-10-04 21:05:28 +0330

    resources/views/role/create.blade.php

--- resources/views/role/create.blade.php
+++ resources/views/role/create.blade.php
@@ -1,15 +1,15 @@
-@extends('layout.app')
-@section('content')
-    <div class="m-5">
-        <h5>create <role></role></h5>
-        <form action="{{route('role.post.create')}}" method="post">
-              @csrf
-            <div class="form-grope">
-                <label for="">Name </label>
-                    <input type="text" name="name" value="{{old('name')}}">
-            </div>
-            <button type="submit" class="btn btn-success">Create</button>
-        </form>
+{{--@extends('layout.app')--}}
+{{--@section('content')--}}
+{{--    <div class="m-5">--}}
+{{--        <h5>create role</h5>--}}
+{{--        <form action="{{route('roles.post.create')}}" method="post">--}}
+{{--              @csrf--}}
+{{--            <div class="form-grope">--}}
+{{--                <label for="">Name </label>--}}
+{{--                    <input type="text" name="name" value="{{old('name')}}">--}}
+{{--            </div>--}}
+{{--            <button type="submit" class="btn btn-success">Create</button>--}}
+{{--        </form>--}}
 
-    </div>
-@endsection
+{{--    </div>--}}
+{{--@endsection--}}
