a159b03b9347980a15686b1ec4f8cfffdad36c35
84arshia84 <arshiia.a84@gmail.com>
2023-10-09 14:45:54 +0330

    create RoleController

--- app/Http/Controllers/RoleController.php
+++ app/Http/Controllers/RoleController.php
@@ -2,13 +2,14 @@
 
 namespace App\Http\Controllers;
 
+use Illuminate\Http\JsonResponse;
 use Illuminate\Http\Request;
 use Spatie\Permission\Models\Permission;
 use Spatie\Permission\Models\Role;
 
 class RoleController extends Controller
 {
-    public function all_roles()
+    public function all_roles(): JsonResponse
     {
         $roles = Role::get();
         return response()->json($roles);
@@ -18,29 +19,29 @@ class RoleController extends Controller
         Role::create(['name' => $request->name]);
         return response()->json('Role Created');
     }
-    public function all_permissions()
+    public function all_permission()
     {
         $permissions = Permission::get();
         return response()->json($permissions);
     }
-    public function add_Permission(Request $request)
+    public function add_permissions(Request $request)
     {
         Permission::create(['name' => $request->name]);
         return response()->json('Permission Created');
     }
-    public function getAssignRole()
-    {
-        $roles = Role::get();
-        $permissions = Permission::get();
-        return response()->json([$roles,$permissions]);
-    }
-
+//    public function getAssignRole()
+//    {
+//        $roles = Role::get();
+//        $permissions = Permission::get();
+//        return response()->json([$roles,$permissions]);
+//    }
+ //
     public function postAssignRole(Request $request)
     {
         $role = Role::findById($request->role_id);
-        $permission = Permission::findById($request->permission_id);
+//        $permission = Permission::findById($request->permission_id);
 
-        $role->givePermissionTo($permission);
+        $role->syncPermissions($request->permission_id);
         return response()->json('successfully role assigned');
         //$role->syncpermissions with checkbox
     }
